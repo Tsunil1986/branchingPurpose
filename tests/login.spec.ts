f24bd49890d3e2bde595b83fc81008bdbfbee2cb
Sunil <tsunilkumar1988@gmail.com>
2026-07-03 18:11:01 +0530

    editied

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -3,5 +3,6 @@ import {test} from '@playwright/test'
 test('Login Page',async ({page})=>{
     await page.goto('https://www.google.com/')
     await page.waitForTimeout(5000)
+    console.log("Branching")
 
 })
